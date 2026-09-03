cdc6405199ff27d13448f10cf78ec391333eaee5
Afsal <afzytk@gmail.com>
2026-09-04 03:05:25 +0530

    style: update spacing and rounding

--- components/SkillsSection.tsx
+++ components/SkillsSection.tsx
@@ -2,15 +2,15 @@ import { skillCategories } from "@/lib/skills";
 
 export const SkillsSection = () => {
   return (
-    <div className="p-5">
-      <p className="text-4xl">Skills</p>
+    <div>
+      <p className="text-4xl m-4">Skills</p>
       {skillCategories.map((category) => (
         <div key={category.name}>
-          <h3>{category.name}</h3>
+          <h3 className="text-2xl m-4">{category.name}</h3>
           {category.skills.map((skill) => (
             <span
               key={skill}
-              className="px-3 py-1 text-sm bg-neutral-800 text-neutral-200 rounded-full m-2"
+              className="p-3 text-sm bg-neutral-800 text-neutral-200 rounded-md m-2"
             >
               {skill}
             </span>
